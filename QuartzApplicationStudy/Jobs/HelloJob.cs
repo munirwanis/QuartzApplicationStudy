8bf0f3d1e523307d85321923e76e34166f5640f0
Munir Wanis <fallen.angel.pp@gmail.com>
2016-06-06 19:03:46 -0300

    :construction: Adds first job

---
+++ QuartzApplicationStudy/Jobs/HelloJob.cs
@@ -0,0 +1,10 @@
+﻿using Quartz;
+using System;
+
+namespace QuartzApplicationStudy.Jobs {
+    public class HelloJob : IJob {
+        public void Execute(IJobExecutionContext context) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- QuartzApplicationStudy/Program.cs
+++ QuartzApplicationStudy/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Quartz;
+using Quartz.Impl;
+using QuartzApplicationStudy.Jobs;
+using System;
+using System.Threading;
 
 namespace QuartzApplicationStudy {
     class Program {
         static void Main(string[] args) {
+            Common.Logging.LogManager.Adapter = new Common.Logging.Simple.ConsoleOutLoggerFactoryAdapter { Level = Common.Logging.LogLevel.Info };
+            try {
+                #region Starting Scheduler
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                scheduler.Start();
+                #endregion
+
+                #region Creating a Job
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+                #endregion
+
+                #region Triggering Job
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("job1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(1)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using the trigger
+                scheduler.ScheduleJob(job, trigger);
+                #endregion
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                #region Terminating Scheduler
+                scheduler.Shutdown();
+                #endregion
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
         }
     }
 }
